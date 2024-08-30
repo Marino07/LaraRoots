0f4685d91dad4839b13af4d432ddf6deccc56aae
Marino <marino.pusic.5@gmail.com>
2024-08-30 20:36:30 +0200

    factories

--- app/Models/Job.php
+++ app/Models/Job.php
@@ -1,10 +1,12 @@
 <?
 namespace App\Models;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Support\Arr;
 use Illuminate\Database\Eloquent\Model;
 
 
 class Job extends Model{
+    use HasFactory;
    protected $table = 'job_listenings';
    protected $fillable = [
     'title',
