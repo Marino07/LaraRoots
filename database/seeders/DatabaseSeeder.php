129e6ae130aef1ea12d0a9d60e811ca2614ad2a2
Marino <marino.pusic.5@gmail.com>
2024-09-01 15:51:23 +0200

    seeders

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -13,11 +13,15 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
+        $this->call([
+            JobSeeder::class,
+            EmployerSeeder::class,
+            PostSeeder::class,
+        ]);
         // User::factory(10)->create();
-
         User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
+            'name' => 'Marino Pusic',
+            'email' => 'marino@example.com',
         ]);
     }
 }
